f1338412c3a080a129afc02e539c9b97cbd577a0
DawnGate <lam99quyen@gmail.com>
2023-08-31 10:37:33 +0700

    config: update config setting for components

---
+++ README.md
@@ -0,0 +1,21 @@
+# Process
+
+1. build package
+   yarn run build
+
+2. Test example
+   In react-chat
+   yarn run link-local
+   yarn run dev
+
+In example folder
+yarn run link-local-lib
+yarn run start
+
+After develop
+
+in example folder
+yarn run unlink-local-lib
+
+in react-chat
+yarn run unlink-local
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+
 import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
